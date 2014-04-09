cdceb84365e1d80062f4da236e673d0faf535258
Jeremy Nelson <jermnelson.gmail.com>
2014-04-09 13:28:12 -0600

    Added pycon 2014 poster

--- dispatcher.py
+++ dispatcher.py
@@ -23,12 +23,14 @@ sys.path.append(DISPATCH_ROOT)
 coasl_server2014 = importlib.import_module("coasl-rda-linked-data.server", None)
 lita_server2014 = importlib.import_module("lita-library-linked-data.server", None)
 nextlibsys_server2014 = importlib.import_module("next-library-systems", None)
+pycon_poster2014 = importlib.import_module('pycon-2014-poster', None)
 
 application = DispatcherMiddleware(
     publisher_app,
     {'/coasl-webinar-2014': coasl_server2014.app,
      '/lita-webinar-2014': lita_server2014.app,
-     '/next-library-systems-2014': nextlibsys_server2014.app})
+     '/next-library-systems-2014': nextlibsys_server2014.app,
+     '/pycon-2014-poster': pycon_poster2014.poster})
 
 def main():
     run_simple('0.0.0.0', 8080, application, use_reloader=True)
