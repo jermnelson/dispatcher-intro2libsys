be6ec5a99e3e62e3c95ea51fcb19647fa3ec86e5
Jeremy Nelson <jermnelson.gmail.com>
2015-04-07 17:41:42 -0600

    added NISO webinar

--- dispatcher.py
+++ dispatcher.py
@@ -1,14 +1,16 @@
-#-------------------------------------------------------------------------------
-# Name:        dispatcher
-# Purpose:     This class dispaches web request to various applications running
-#              on the intro2libys.info domain
-#
-# Author:      Jeremy Nelson
-#
-# Created:     2014-01-21
-# Copyright:   (c) Jeremy Nelson 2014
-# Licence:     GPL v.2
-#-------------------------------------------------------------------------------
+"""
+ Name:        dispatcher
+
+
+ Purpose:     This class dispaches web request to various applications running
+              on the intro2libys.info domain
+
+ Author:      Jeremy Nelson
+
+ Created:     2014-01-21
+ Copyright:   (c) Jeremy Nelson 2014, 2015
+ Licence:     GPL v.2
+"""
 import importlib
 import os
 import sys
@@ -25,6 +27,7 @@ code4lib_talk2015 = importlib.import_module("code4lib-2015-talk.talk", None)
 dlf_forum_poster2014 = importlib.import_module("dlf-forum-2014-poster", None)
 lita_server2014 = importlib.import_module("lita-library-linked-data.server", None)
 nextlibsys_server2014 = importlib.import_module("next-library-systems", None)
+niso_webinar2015 = importlib.import_module("niso-2015-webinar", None)
 pycon_poster2014 = importlib.import_module('pycon-2014-poster', None)
 ##islandora_camp2014 = importlib.import_module('islandora-camp-2014', None)
 intro_redis = importlib.import_module('introduction-to-redis', None)
@@ -39,10 +42,11 @@ application = DispatcherMiddleware(
      '/introduction-to-redis': intro_redis.app,
   ##   '/islandora-camp-2014': islandora_camp2014.presentation,
      '/next-library-systems-2014': nextlibsys_server2014.app,
+     '/niso-2015-webinar': niso_webinar2015.app,
      '/pycon-2014-poster': pycon_poster2014.poster})
 
 def main():
-    run_simple('0.0.0.0', 8080, application, use_reloader=True)
+    run_simple('0.0.0.0', 8081, application, use_reloader=True)
 
 
 if __name__ == '__main__':
