422f49cbe3b4f9d09f80e81d589bd8dec5371db7
Jeremy Nelson <jermnelson.gmail.com>
2015-09-27 13:32:20 -0600

    Added Pycon Japan Presentation

--- dispatcher.py
+++ dispatcher.py
@@ -32,6 +32,7 @@ lita_server2014 = importlib.import_module("lita-library-linked-data.server", Non
 nextlibsys_server2014 = importlib.import_module("next-library-systems", None)
 niso_webinar2015 = importlib.import_module("niso-2015-webinar", None)
 or_talk2015 = importlib.import_module("open-repository-2015", None)
+pyconjp_2015 = importlib.import_module("pyconjp-2015", None)
 pycon_poster2014 = importlib.import_module('pycon-2014-poster', None)
 ##islandora_camp2014 = importlib.import_module('islandora-camp-colorado', None)
 intro_redis = importlib.import_module('introduction-to-redis', None)
@@ -51,7 +52,8 @@ application = DispatcherMiddleware(
      '/next-library-systems-2014': nextlibsys_server2014.app,
      '/niso-2015-webinar': niso_webinar2015.app,
      '/or-2015': or_talk2015.app,
-     '/pycon-2014-poster': pycon_poster2014.poster})
+     '/pycon-2014-poster': pycon_poster2014.poster,
+     '/pycon-jp-2015': pyconjp_2015.app})
 
 def main():
     run_simple('0.0.0.0', 8500, application, use_reloader=True, use_debugger=True)
